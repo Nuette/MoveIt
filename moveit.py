53b80abc96673493267b676486b9501709cba5ac
Nuette <nuette.heyns@gmail.com>
2024-10-11 09:37:43 +0200

    Changed game name, added sound

--- moveit.py
+++ moveit.py
@@ -9,6 +9,8 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("MoveIt!")
+
 
 jump_sound = pygame.mixer.Sound('jump.wav')
 move_sound = pygame.mixer.Sound('move.wav')
